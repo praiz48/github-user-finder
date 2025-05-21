e3a3aa2d1b30f5dc47eb6ff34991904a1bf78bf6
praiz48 <praisekolawole80@gmail.com>
2025-05-21 04:58:35 -1100

    vercel bugs fixed

--- git-find/src/App.tsx
+++ git-find/src/App.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { useState } from "react";
 import { useQuery } from "@tanstack/react-query";
 import SearchCop from "./components/SearchCop";
--- git-find/src/components/DisplayComp.tsx
+++ git-find/src/components/DisplayComp.tsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 interface UserData {
   name: string;
   login: string;
